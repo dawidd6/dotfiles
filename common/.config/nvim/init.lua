7cca18f758207aca0065905ff692c5a574b7e167
Dawid Dziurla <dawidd0811@gmail.com>
2025-03-04 08:30:46 +0100

    Revert "nvim: make room for vscode"
    This reverts commit 6b6017cd57159b4440986a7a24b907da726a6eaf.

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,3 +1,20 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Leader key
 vim.g.mapleader = " "
 
@@ -23,72 +40,50 @@ vim.opt.clipboard = "unnamedplus"
 vim.keymap.set({ "n", "v" }, "x", '"_x', { silent = true })
 vim.keymap.set({ "n", "v" }, "X", '"_X', { silent = true })
 vim.keymap.set("v", "p", '"_dP', { silent = true })
-vim.keymap.set('n', ',', ':bprevious<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '.', ':bnext<CR>', { noremap = true, silent = true })
 
-if vim.g.vscode then
-else
-	-- Bootstrap lazy.nvim
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-		if vim.v.shell_error ~= 0 then
-			vim.api.nvim_echo({
-				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-				{ out, "WarningMsg" },
-				{ "\nPress any key to exit..." },
-			}, true, {})
-			vim.fn.getchar()
-			os.exit(1)
-		end
-	end
-	vim.opt.rtp:prepend(lazypath)
-
-	-- Plugin setup using lazy.nvim
-	require("lazy").setup({
-		{ "akinsho/bufferline.nvim", config = true },
-		{ "akinsho/git-conflict.nvim", config = true },
-		{ "lewis6991/gitsigns.nvim", config = true },
-		{ "NMAC427/guess-indent.nvim", config = true },
-		{ "nvim-lualine/lualine.nvim", config = true },
-		{ "folke/noice.nvim", config = true },
-		{ "johnfrankmorgan/whitespace.nvim", config = true },
-		{ "kyazdani42/nvim-web-devicons", config = true },
-		{ "windwp/nvim-autopairs", config = true },
-		{ "folke/which-key.nvim", config = true },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{
-			"hrsh7th/nvim-cmp",
-			config = function()
-				local cmp = require("cmp")
-				cmp.setup({
-					mapping = cmp.mapping.preset.insert({
-						["<CR>"] = cmp.mapping.confirm({ select = true }),
-						["<Tab>"] = cmp.mapping.confirm({ select = true }),
-						["<Up>"] = cmp.mapping.select_prev_item(),
-						["<Down>"] = cmp.mapping.select_next_item(),
-					}),
-					sources = cmp.config.sources({
-						{ name = "path" },
-						{ name = "buffer" },
-					}),
-				})
-			end,
-		},
-		{
-			"goolord/alpha-nvim",
-			config = function()
-				require("alpha").setup(require("alpha.themes.startify").config)
-			end,
-		},
-		{
-			"Mofiqul/vscode.nvim",
-			lazy = false,
-			config = function()
-				vim.cmd.colorscheme("vscode")
-			end,
-		},
-	})
-end
+-- Plugin setup using lazy.nvim
+require("lazy").setup({
+	{ "akinsho/bufferline.nvim", config = true },
+	{ "akinsho/git-conflict.nvim", config = true },
+	{ "lewis6991/gitsigns.nvim", config = true },
+	{ "NMAC427/guess-indent.nvim", config = true },
+	{ "nvim-lualine/lualine.nvim", config = true },
+	{ "folke/noice.nvim", config = true },
+	{ "johnfrankmorgan/whitespace.nvim", config = true },
+	{ "kyazdani42/nvim-web-devicons", config = true },
+	{ "windwp/nvim-autopairs", config = true },
+	{ "folke/which-key.nvim", config = true },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<Up>"] = cmp.mapping.select_prev_item(),
+					["<Down>"] = cmp.mapping.select_next_item(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "path" },
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		config = function()
+			vim.cmd.colorscheme("vscode")
+		end,
+	},
+})
