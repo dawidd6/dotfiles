9c89cfed77d63e485e0051139744ba1e772b19d9
Dawid Dziurla <dawidd0811@gmail.com>
2026-05-22 20:59:48 +0200

    nvim: update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -15,10 +15,22 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Leader key
-vim.g.mapleader = " "
-
 -- General settings
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.mouse = "a"
+vim.opt.showmode = false
 vim.opt.expandtab = true
 vim.opt.gdefault = true
 vim.opt.ignorecase = true
@@ -40,8 +52,8 @@ vim.opt.clipboard = "unnamedplus"
 vim.keymap.set({ "n", "v" }, "x", '"_x', { silent = true })
 vim.keymap.set({ "n", "v" }, "X", '"_X', { silent = true })
 vim.keymap.set("v", "p", '"_dP', { silent = true })
-vim.keymap.set('n', ',', ':bprevious<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '.', ':bnext<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", ",", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", ".", ":bnext<CR>", { noremap = true, silent = true })
 
 -- Plugin setup using lazy.nvim
 require("lazy").setup({
@@ -50,30 +62,57 @@ require("lazy").setup({
 	{ "lewis6991/gitsigns.nvim", config = true },
 	{ "NMAC427/guess-indent.nvim", config = true },
 	{ "nvim-lualine/lualine.nvim", config = true },
-	-- { "folke/noice.nvim", config = true },
+	--	{ "folke/noice.nvim", config = true },
 	{ "johnfrankmorgan/whitespace.nvim", config = true },
 	{ "kyazdani42/nvim-web-devicons", config = true },
 	{ "windwp/nvim-autopairs", config = true },
 	{ "folke/which-key.nvim", config = true },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
+	--	{ "hrsh7th/cmp-buffer" },
+	--	{ "hrsh7th/cmp-path" },
+	--	{
+	--		"hrsh7th/nvim-cmp",
+	--		config = function()
+	--			local cmp = require("cmp")
+	--			cmp.setup({
+	--				mapping = cmp.mapping.preset.insert({
+	--					["<CR>"] = cmp.mapping.confirm({ select = true }),
+	--					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+	--					["<Up>"] = cmp.mapping.select_prev_item(),
+	--					["<Down>"] = cmp.mapping.select_next_item(),
+	--				}),
+	--				sources = cmp.config.sources({
+	--					{ name = "path" },
+	--					{ name = "buffer" },
+	--				}),
+	--			})
+	--		end,
+	--	},
 	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }),
-					["<Up>"] = cmp.mapping.select_prev_item(),
-					["<Down>"] = cmp.mapping.select_next_item(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "path" },
-					{ name = "buffer" },
-				}),
-			})
-		end,
+		"saghen/blink.cmp",
+
+		opts = {
+			keymap = {
+				["<CR>"] = { "accept", "fallback" },
+				["<Tab>"] = { "accept", "fallback" },
+
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<Left>"] = { "hide", "fallback" },
+				["<Right>"] = { "hide", "fallback" },
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		},
 	},
 	{
 		"goolord/alpha-nvim",
@@ -91,7 +130,7 @@ require("lazy").setup({
 }, {
 	performance = {
 		rtp = {
-			reset = false
-		}
-	}
+			reset = false,
+		},
+	},
 })
