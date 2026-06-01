15da2d46b0206e9e9d024c10a08e65235b38c7d7
Dawid Dziurla <dawidd0811@gmail.com>
2026-06-01 18:07:42 +0200

    update

--- install.sh
+++ install.sh
@@ -2,55 +2,103 @@
 
 set -euo pipefail
 
-if test $EUID -eq 0 && ! test -e /run/.containerenv; then
-    echo "Do not run this as root!"
-    exit 127
-fi
+dotfiles_user=dawidd6
 
-# APT
-sudo apt install -y podman
+brew_packages=(
+    age
+    chezmoi
+    diff-so-fancy
+    diffoscope
+    dos2unix
+    eza
+    fd
+    fish
+    fzf
+    git
+    glow
+    helm
+    htop
+    ipcalc
+    k9s
+    kubectl
+    kustomize
+    make
+    neovim
+    ripgrep
+    shellcheck
+    sops
+    starship
+    stylua
+    trash-cli
+    wl-clipboard
+    zoxide
+)
 
-# Homebrew
-sudo apt install -y build-essential curl file git procps
-if ! test -e /home/linuxbrew/.linuxbrew/bin/brew; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-brew install age chezmoi diff-so-fancy diffoscope dos2unix eza fd fish fzf git glow helm htop k9s kubectl make neovim ripgrep shellcheck sops starship stylua trash-cli wl-clipboard zoxide
-if ! grep -q /home/linuxbrew/.linuxbrew/bin/brew /etc/shells; then
-    echo /home/linuxbrew/.linuxbrew/bin/fish | sudo tee -a /etc/shells
-fi
-sudo chsh -s /home/linuxbrew/.linuxbrew/bin/fish "$(whoami)"
+apt_packages=(
+    podman
+)
 
-# Dotfiles
-if ! test -d ~/.local/share/chezmoi; then
-    chezmoi init --apply dawidd6
-fi
-
-# Desktop
-if command -v gnome-shell &>/dev/null; then
-    if ! test -d ~/.local/share/fonts; then
-        cd /tmp
-        mkdir -p ~/.local/share/fonts
-        wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.zip
-        unzip -o UbuntuMono.zip -d ~/.local/share/fonts
+function check() {
+    if test $EUID -eq 0 && ! test -e /run/.containerenv; then
+        echo "Do not run this as root!"
+        exit 127
     fi
+}
 
-    gnome-extensions disable ubuntu-dock@ubuntu.com
-    gnome-extensions disable ding@rastersoft.com
+function setup_apt() {
+    sudo apt install -y "${apt_packages[@]}"
+}
 
-    gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
-    gsettings set org.gnome.desktop.interface monospace-font-name 'UbuntuMono Nerd Font 13'
-    gsettings set org.gnome.desktop.interface document-font-name 'Sans 11'
-    gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Ubuntu Bold 11'
-    gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
-    gsettings set org.gnome.settings-daemon.plugins.power idle-brightness 100
+function setup_brew() {
+    sudo apt install -y build-essential curl file git procps
+    if ! test -e /home/linuxbrew/.linuxbrew/bin/brew; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+    brew install "${brew_packages[@]}"
+    if ! grep -q /home/linuxbrew/.linuxbrew/bin/brew /etc/shells; then
+        echo /home/linuxbrew/.linuxbrew/bin/fish | sudo tee -a /etc/shells
+    fi
+    sudo chsh -s /home/linuxbrew/.linuxbrew/bin/fish "$(whoami)"
+}
 
-    sudo pam-auth-update --enable fprintd
+function setup_dotfiles() {
+    if ! chezmoi git rev-parse &>/dev/null; then
+        chezmoi init --apply "$dotfiles_user"
+    fi
+}
 
-    sudo apt purge fonts-noto-core
-    sudo apt install fonts-ubuntu-classic
+function setup_desktop() {
+    if command -v gnome-shell &>/dev/null; then
+        if ! test -d ~/.local/share/fonts; then
+            cd /tmp
+            mkdir -p ~/.local/share/fonts
+            wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.zip
+            unzip -o UbuntuMono.zip -d ~/.local/share/fonts
+        fi
 
-    sudo apt install -y flatpak
-    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-fi
+        gnome-extensions disable ubuntu-dock@ubuntu.com
+        gnome-extensions disable ding@rastersoft.com
+
+        gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
+        gsettings set org.gnome.desktop.interface monospace-font-name 'UbuntuMono Nerd Font 13'
+        gsettings set org.gnome.desktop.interface document-font-name 'Sans 11'
+        gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Ubuntu Bold 11'
+        gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+        gsettings set org.gnome.settings-daemon.plugins.power idle-brightness 100
+
+        sudo pam-auth-update --enable fprintd
+
+        sudo apt purge fonts-noto-core
+        sudo apt install fonts-ubuntu-classic
+
+        sudo apt install -y flatpak
+        sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    fi
+}
+
+check
+setup_apt
+setup_brew
+setup_dotfiles
+setup_desktop
