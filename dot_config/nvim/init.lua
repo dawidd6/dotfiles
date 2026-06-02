f36a2d17b842bfa63560fb49b70e69a88bcfb938
Dawid Dziurla <dawidd0811@gmail.com>
2026-06-02 11:12:29 +0200

    Update diagnostic configuration in init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -173,6 +173,5 @@ vim.api.nvim_create_autocmd("CursorHold", {
 
 -- Diagnostics
 vim.diagnostic.config({
-	virtual_text = true,
-	virtual_lines = true,
+	virtual_lines = { current_line = true }
 })
