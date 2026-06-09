251dd5ff6048bdf6c2635fb5f1a6642019343cee
Dawid Dziurla <dawidd0811@gmail.com>
2026-06-09 18:17:33 +0200

    nvim: use plugins again

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,55 @@
+-- Plugin for nice icons
+vim.pack.add({ { src = "https://github.com/nvim-tree/nvim-web-devicons" } })
+
+-- Plugin for handling whitespace
+vim.pack.add({ { src = "https://github.com/johnfrankmorgan/whitespace.nvim" } })
+require("whitespace-nvim").setup()
+
+-- Plugin for highlighting special comments
+vim.pack.add({ { src = "https://github.com/folke/todo-comments.nvim" } })
+require("todo-comments").setup()
+
+-- Plugin for buffer line
+vim.pack.add({ { src = "https://github.com/akinsho/bufferline.nvim" } })
+require("bufferline").setup()
+
+-- Plugin for status line
+vim.pack.add({ { src = "https://github.com/nvim-lualine/lualine.nvim" } })
+require("lualine").setup()
+
+-- Plugin for git integration
+vim.pack.add({ { src = "https://github.com/lewis6991/gitsigns.nvim" } })
+require("gitsigns").setup()
+
+-- Plugin for git conflicts handling
+vim.pack.add({ { src = "https://github.com/akinsho/git-conflict.nvim" } })
+require("git-conflict").setup()
+
+-- Plugin for guessing indentation
+vim.pack.add({ { src = "https://github.com/NMAC427/guess-indent.nvim" } })
+require("guess-indent").setup()
+
+-- Plugin for completion
+vim.pack.add({ "https://github.com/saghen/blink.lib", "https://github.com/saghen/blink.cmp" })
+require("blink.cmp").setup({
+    fuzzy = {
+        implementation = "lua",
+    },
+    keymap = {
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<Left>"] = { "hide", "fallback" },
+        ["<Right>"] = { "hide", "fallback" },
+    },
+})
+
+-- Plugin for theme/colorcheme
+vim.pack.add({ { src = "https://github.com/Mofiqul/vscode.nvim" } })
+require("vscode").setup()
+vim.cmd.colorscheme("vscode")
+
 -- Keymaps
 vim.keymap.set({ "n", "v" }, "x", '"_x')
 vim.keymap.set({ "n", "v" }, "X", '"_X')
