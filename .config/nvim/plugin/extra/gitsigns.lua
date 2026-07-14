bdf7918f251f861a9899c37a0cb9437f780e7d08
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-14 13:44:13 +0200

    nvim: update

--- .config/nvim/plugin/extra/gitsigns.lua
+++ .config/nvim/plugin/extra/gitsigns.lua
@@ -6,6 +6,12 @@ local gitsigns = require("gitsigns")
 
 gitsigns.setup()
 
+vim.api.nvim_create_user_command("GitToggleLineBlame", function()
+	gitsigns.toggle_current_line_blame({ full = true })
+end, {
+	desc = "Toggle git line blame",
+})
+
 vim.keymap.set("n", "gb", function()
 	gitsigns.blame_line({ full = true })
 end, { silent = true, desc = "Blame line" })
