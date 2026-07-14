f6cdddda2a527a2dc956ece6e366e5a402dea8f7
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-14 13:47:03 +0200

    nvim: update

--- .config/nvim/plugin/extra/conform.lua
+++ .config/nvim/plugin/extra/conform.lua
@@ -2,7 +2,9 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim" },
 })
 
-require("conform").setup({
+local conform = require("conform")
+
+conform.setup({
 	format_on_save = function(bufnr)
 		if vim.b[bufnr].disable_autoformat then
 			return
@@ -26,3 +28,15 @@ vim.api.nvim_create_user_command("FormatEnable", function()
 end, {
 	desc = "Enable autoformat-on-save",
 })
+
+vim.api.nvim_create_user_command("Format", function(args)
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	conform.format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
