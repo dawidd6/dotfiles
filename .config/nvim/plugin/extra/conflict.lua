cd12161bf8d575e278f1c76bca5f5c5ae33bd2f8
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-15 14:36:14 +0200

    update

--- .config/nvim/plugin/extra/conflict.lua
+++ .config/nvim/plugin/extra/conflict.lua
@@ -2,11 +2,4 @@ vim.pack.add({
 	{ src = "https://github.com/niekdomi/conflict.nvim" },
 })
 
-require("conflict").setup({
-	default_mappings = {
-		current = "<LocalLeader>cc",
-		incoming = "<LocalLeader>ci",
-		both = "<LocalLeader>cb",
-		base = "<LocalLeader>cB",
-	},
-})
+require("conflict").setup()
