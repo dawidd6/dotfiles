ecc00ee647e5843f6750969e5d4313674c4997b3
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-17 16:09:19 +0200

    nvim: update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,8 @@ vim.loader.enable()
 vim.cmd("runtime! lua/core/*.lua")
 
 if vim.g.vscode then
-	vim.cmd("runtime! lua/plugin/nvim-surround.lua")
+	vim.cmd("runtime! lua/plugin/autopairs.lua")
+	vim.cmd("runtime! lua/plugin/surround.lua")
 else
 	vim.cmd("runtime! lua/plugin/*.lua")
 end
