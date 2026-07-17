0bb7f8a71e4a1739ea3d07c26962ec0731d1feea
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-17 19:30:02 +0200

    nvim: fix snapped vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,8 +3,10 @@ vim.loader.enable()
 vim.cmd("runtime! lua/core/*.lua")
 
 if vim.g.vscode then
-	vim.cmd("runtime! lua/plugin/autopairs.lua")
-	vim.cmd("runtime! lua/plugin/surround.lua")
+	vim.env.XDG_DATA_HOME = (vim.env.SNAP_REAL_HOME or vim.env.HOME) .. "/.local/share"
+	vim.opt.packpath:prepend(vim.env.XDG_DATA_HOME .. "/nvim/site")
+	vim.cmd("runtime! lua/extra/autopairs.lua")
+	vim.cmd("runtime! lua/extra/surround.lua")
 else
-	vim.cmd("runtime! lua/plugin/*.lua")
+	vim.cmd("runtime! lua/extra/*.lua")
 end
