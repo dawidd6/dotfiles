1f416a758bee029a83bdb70cac398ab3c1a651bc
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-17 19:39:12 +0200

    nvim: delete snap workaround

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,8 +3,6 @@ vim.loader.enable()
 vim.cmd("runtime! lua/core/*.lua")
 
 if vim.g.vscode then
-	vim.env.XDG_DATA_HOME = (vim.env.SNAP_REAL_HOME or vim.env.HOME) .. "/.local/share"
-	vim.opt.packpath:prepend(vim.env.XDG_DATA_HOME .. "/nvim/site")
 	vim.cmd("runtime! lua/extra/autopairs.lua")
 	vim.cmd("runtime! lua/extra/surround.lua")
 else
