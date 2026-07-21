cb553a7626e05fcc41a869b7fff3c2be19b81f32
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-21 13:19:10 +0200

    fish: no icons eza

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -122,7 +122,7 @@ if status is-interactive
     alias vim nvim
     alias fd 'fd --exclude=.git/ --no-ignore --hidden'
     alias rg 'rg --glob=!.git/ --no-ignore --hidden --smart-case'
-    alias eza 'eza --group-directories-first --group --header --time-style=long-iso --icons'
+    alias eza 'eza --group-directories-first --group --header --time-style=long-iso'
     alias l eza
     alias ls eza
     alias la 'eza -a'
