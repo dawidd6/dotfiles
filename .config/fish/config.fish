d5f077d5476c8a46964ecd61d252e7f6ec3ceff9
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-23 11:11:51 +0200

    fish: update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -122,9 +122,8 @@ if status is-interactive
     alias v nvim
     alias vi nvim
     alias vim nvim
-    alias diffoscope 'diffoscope --text-color=always  --exclude-directory-metadata=recursive'
-    alias fd 'fd --color=always --exclude=.git/ --no-ignore --hidden'
-    alias rg 'rg --color=always --glob=!.git/ --heading --line-number --no-ignore --hidden --smart-case'
+    alias fd 'fd --exclude=.git/ --no-ignore --hidden'
+    alias rg 'rg --glob=!.git/ --heading --line-number --no-ignore --hidden --smart-case'
     alias eza 'eza --group-directories-first --group --header --time-style=long-iso'
     alias l eza
     alias ls eza
