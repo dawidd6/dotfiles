0d82b1ef56b3242b21c9ec59c822a334cd2945f7
Dawid Dziurla <dawidd0811@gmail.com>
2026-07-28 23:51:29 +0200

    nvim: update

--- .config/nvim/lua/extra/dial.lua
+++ .config/nvim/lua/extra/dial.lua
@@ -6,29 +6,18 @@ local augend = require("dial.augend")
 
 require("dial.config").augends:register_group({
 	default = {
-		augend.integer.alias.decimal_int,
-		augend.integer.alias.hex,
-		augend.date.alias["%Y-%m-%d"],
-		augend.date.alias["%d/%m/%Y"],
-		augend.date.alias["%H:%M:%S"],
-		augend.semver.alias.semver,
-		augend.hexcolor.new({
-			case = "lower",
-		}),
+		augend.constant.alias.en_weekday,
+		augend.constant.alias.en_weekday_full,
 		augend.constant.alias.bool,
+		augend.constant.alias.Bool,
 		augend.constant.new({
 			elements = { "yes", "no" },
-			word = true,
-			cyclic = true,
 		}),
 		augend.constant.new({
 			elements = { "and", "or" },
-			word = true,
-			cyclic = true,
 		}),
 		augend.case.new({
-			types = { "camelCase", "snake_case" },
-			cyclic = true,
+			types = { "camelCase", "snake_case", "SCREAMING_SNAKE_CASE", "PascalCase" },
 		}),
 	},
 })
