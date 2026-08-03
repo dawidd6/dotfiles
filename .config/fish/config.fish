6a1e051c7539e76b2c3da6477dc48e96cf5c41cb
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-03 22:52:56 +0200

    fish: INTERACTIVE

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 # Exports
-status is-interactive && export LANG='C.UTF-8'
 export PAGER='less'
 export EDITOR='nvim'
 export XDG_CONFIG_HOME="$HOME/.config"
@@ -14,137 +13,144 @@ export RIPGREP_CONFIG_PATH="$HOME/.config/rg/config"
 
 # Sources
 /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
-fzf --fish | source
-starship init fish | source
-zoxide init fish --cmd cd | source
 
 # PATH
 fish_add_path --path --global --move "$HOME/.local/bin"
 
-# Colors
-set fish_color_command green
-set fish_color_param normal
-set fish_color_error red --bold
-set fish_color_normal normal
-set fish_color_comment brblack
-set fish_color_quote yellow
+if status is-interactive
+    # Exports
+    export LANG='C.UTF-8'
 
-# Functions
-function fish_greeting
-end
-function cdt --description 'Tere based cd command'
-    set --local result (command tere $argv)
-    test -n "$result" && cd -- "$result"
-end
-function cdr --description 'Git root based cd command'
-    set --local result (command git rev-parse --show-toplevel)
-    test -n "$result" && cd -- "$result"
-end
-function cdp --description 'Clipboard based cd command'
-    set --local result (command wl-paste)
-    test -n "$result" && cd -- "$result"
-end
-function __home_git_env --on-variable PWD
-    if test "$PWD" = "$HOME"
-        set -gx GIT_DIR "$HOME/.dotfiles"
-        set -gx GIT_WORK_TREE "$HOME"
-    else
-        set -q GIT_DIR && test "$GIT_DIR" = "$HOME/.dotfiles" && set -e GIT_DIR
-        set -q GIT_WORK_TREE && test "$GIT_WORK_TREE" = "$HOME" && set -e GIT_WORK_TREE
+    # Sources
+    fzf --fish | source
+    starship init fish | source
+    zoxide init fish --cmd cd | source
+
+    # Colors
+    set fish_color_command green
+    set fish_color_param normal
+    set fish_color_error red --bold
+    set fish_color_normal normal
+    set fish_color_comment brblack
+    set fish_color_quote yellow
+
+    # Functions
+    function fish_greeting
     end
-end
-__home_git_env
+    function cdt --description 'Tere based cd command'
+        set --local result (command tere $argv)
+        test -n "$result" && cd -- "$result"
+    end
+    function cdr --description 'Git root based cd command'
+        set --local result (command git rev-parse --show-toplevel)
+        test -n "$result" && cd -- "$result"
+    end
+    function cdp --description 'Clipboard based cd command'
+        set --local result (command wl-paste)
+        test -n "$result" && cd -- "$result"
+    end
+    function __home_git_env --on-variable PWD
+        if test "$PWD" = "$HOME"
+            set -gx GIT_DIR "$HOME/.dotfiles"
+            set -gx GIT_WORK_TREE "$HOME"
+        else
+            set -q GIT_DIR && test "$GIT_DIR" = "$HOME/.dotfiles" && set -e GIT_DIR
+            set -q GIT_WORK_TREE && test "$GIT_WORK_TREE" = "$HOME" && set -e GIT_WORK_TREE
+        end
+    end
+    __home_git_env
 
-# Completions
-complete -c git-multi -w git
-complete -c git-log-preview -w 'git log'
+    # Completions
+    complete -c git-multi -w git
+    complete -c git-log-preview -w 'git log'
 
-# Binds
-bind ctrl-g edit_command_buffer
-bind ctrl-s 'fish_commandline_prepend sudo'
-bind \e\[1\;5C nextd-or-forward-word
-bind \e\[1\;5D prevd-or-backward-word
+    # Binds
+    bind ctrl-g edit_command_buffer
+    bind ctrl-s 'fish_commandline_prepend sudo'
+    bind \e\[1\;5C nextd-or-forward-word
+    bind \e\[1\;5D prevd-or-backward-word
 
-# Abbrs
-abbr C wl-copy
-abbr P wl-paste
-abbr D 'diff-so-fancy | less --tabs 4 -RFX'
-abbr R 'exec fish'
-abbr r reset
-abbr e exit
-abbr g git
-abbr ga 'git add'
-abbr gaa 'git add -A'
-abbr gaf 'git add -f'
-abbr gap 'git add -p'
-abbr gb 'git branch -a'
-abbr gh 'git checkout'
-abbr ghb 'git checkout -b'
-abbr ghm 'git checkout-main-branch'
-abbr ghp 'git checkout -p'
-abbr gc 'git commit -m'
-abbr gca 'git commit --amend --no-edit'
-abbr gcae 'git commit --amend --edit'
-abbr gd 'git diff'
-abbr gdc 'git diff --cached'
-abbr gdn 'git diff --no-index'
-abbr gg 'git grep'
-abbr gl 'git lg'
-abbr glp 'git log-preview'
-abbr gp 'git push'
-abbr gpf 'git push -f'
-abbr gpd 'git push --delete'
-abbr gpp 'git pull-preview'
-abbr gs 'git status -u'
-abbr gt 'git tag'
-abbr gu 'git unadd'
-abbr p podman
-abbr pp 'podman ps'
-abbr pi 'podman images'
-abbr pr 'podman run --rm'
-abbr pri 'podman run --rm -it'
-abbr pe 'podman exec'
-abbr pei 'podman exec -it'
-abbr k kubectl
-abbr ka 'kubectl apply -f'
-abbr kd 'kubectl describe'
-abbr ke 'kubectl exec -it'
-abbr kg 'kubectl get'
-abbr kgy 'kubectl get -o yaml'
-abbr kr 'kubectl run -it --rm --restart=Never --image'
-abbr kc kubectx
-abbr kn kubens
-abbr i incus
-abbr ii 'incus info'
-abbr ie 'incus exec -it $NAME -- sh'
-abbr is 'incus shell'
-abbr id 'incus delete'
-abbr il 'incus list'
-abbr ir 'incus launch --vm -c limits.cpu=4 -c limits.memory=4GiB -d root,size=50GiB $NAME images:'
-abbr im 'incus config device add $NAME share disk source=$PWD path=$PWD'
-abbr ic 'incus console --type vga'
-abbr b brew
-abbr bi 'brew install'
-abbr br 'brew uninstall'
-abbr bl 'brew list'
-abbr bu 'brew upgrade'
-abbr bbd 'brew bundle -g dump --force --formula --cask'
-abbr bbc 'brew bundle -g check --verbose'
-abbr bbi 'brew bundle -g install'
-abbr t tmux
-abbr l ls
-abbr v nvim
-abbr vi nvim
-abbr vim nvim
+    # Abbrs
+    abbr C wl-copy
+    abbr P wl-paste
+    abbr D 'diff-so-fancy | less --tabs 4 -RFX'
+    abbr R 'exec fish'
+    abbr r reset
+    abbr e exit
+    abbr g git
+    abbr ga 'git add'
+    abbr gaa 'git add -A'
+    abbr gaf 'git add -f'
+    abbr gap 'git add -p'
+    abbr gb 'git branch -a'
+    abbr gh 'git checkout'
+    abbr ghb 'git checkout -b'
+    abbr ghm 'git checkout-main-branch'
+    abbr ghp 'git checkout -p'
+    abbr gc 'git commit -m'
+    abbr gca 'git commit --amend --no-edit'
+    abbr gcae 'git commit --amend --edit'
+    abbr gd 'git diff'
+    abbr gdc 'git diff --cached'
+    abbr gdn 'git diff --no-index'
+    abbr gg 'git grep'
+    abbr gl 'git lg'
+    abbr glp 'git log-preview'
+    abbr gp 'git push'
+    abbr gpf 'git push -f'
+    abbr gpd 'git push --delete'
+    abbr gpp 'git pull-preview'
+    abbr gs 'git status -u'
+    abbr gt 'git tag'
+    abbr gu 'git unadd'
+    abbr p podman
+    abbr pp 'podman ps'
+    abbr pi 'podman images'
+    abbr pr 'podman run --rm'
+    abbr pri 'podman run --rm -it'
+    abbr pe 'podman exec'
+    abbr pei 'podman exec -it'
+    abbr k kubectl
+    abbr ka 'kubectl apply -f'
+    abbr kd 'kubectl describe'
+    abbr ke 'kubectl exec -it'
+    abbr kg 'kubectl get'
+    abbr kgy 'kubectl get -o yaml'
+    abbr kr 'kubectl run -it --rm --restart=Never --image'
+    abbr kc kubectx
+    abbr kn kubens
+    abbr i incus
+    abbr ii 'incus info'
+    abbr ie 'incus exec -it $NAME -- sh'
+    abbr is 'incus shell'
+    abbr id 'incus delete'
+    abbr il 'incus list'
+    abbr ir 'incus launch --vm -c limits.cpu=4 -c limits.memory=4GiB -d root,size=50GiB $NAME images:'
+    abbr im 'incus config device add $NAME share disk source=$PWD path=$PWD'
+    abbr ic 'incus console --type vga'
+    abbr b brew
+    abbr bi 'brew install'
+    abbr br 'brew uninstall'
+    abbr bl 'brew list'
+    abbr bu 'brew upgrade'
+    abbr bbd 'brew bundle -g dump --force --formula --cask'
+    abbr bbc 'brew bundle -g check --verbose'
+    abbr bbi 'brew bundle -g install'
+    abbr t tmux
+    abbr l ls
+    abbr v nvim
+    abbr vi nvim
+    abbr vim nvim
 
-# Aliases
-alias rm 'command trash'
-alias ghub 'command gh'
-alias ls 'eza --group-directories-first --group --header --time-style long-iso --all'
-alias ll 'ls --long --icons'
-alias lt 'll --tree --level 2 --ignore-glob .git'
+    # Aliases
+    alias rm 'command trash'
+    alias ghub 'command gh'
+    alias ls 'eza --group-directories-first --group --header --time-style long-iso --all'
+    alias ll 'ls --long --icons'
+    alias lt 'll --tree --level 2 --ignore-glob .git'
 
-# Mux
-if status is-interactive && not set -q TMUX && not set -q VSCODE_INJECTION
-    tmux new-session -A -s main
+    # Mux
+    if not set -q TMUX && not set -q VSCODE_INJECTION
+        tmux new-session -A -s main
+    end
 end
