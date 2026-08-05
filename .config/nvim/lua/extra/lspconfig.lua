7ca4bfa4b9dca7ad997ee3e3a5ad0b5cfd590146
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-06 00:04:17 +0200

    nvim: add yaml.ansible filetypes

--- .config/nvim/lua/extra/lspconfig.lua
+++ .config/nvim/lua/extra/lspconfig.lua
@@ -60,6 +60,21 @@ vim.lsp.config("yamlls", {
 	},
 })
 
+vim.filetype.add({
+	pattern = {
+		[".*/defaults/.*%.ya?ml"] = "yaml.ansible",
+		[".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
+		[".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
+		[".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
+		[".*/playbook.*%.ya?ml"] = "yaml.ansible",
+		[".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
+		[".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
+		[".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+		[".*/tasks/.*%.ya?ml"] = "yaml.ansible",
+		[".*/molecule/.*%.ya?ml"] = "yaml.ansible",
+	},
+})
+
 vim.lsp.enable({
 	"ansiblels",
 	"dockerls",
