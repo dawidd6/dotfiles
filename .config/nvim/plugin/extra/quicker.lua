e358eb91006d88d217b53836c7ed6c0c789101d4
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-11 15:56:07 +0200

    nvim: fix <CR> in quickfix window

--- .config/nvim/plugin/extra/quicker.lua
+++ .config/nvim/plugin/extra/quicker.lua
@@ -2,4 +2,8 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/quicker.nvim" },
 })
 
-require("quicker").setup()
+require("quicker").setup({
+	keys = {
+		{ "<CR>", "<CR>", desc = "Open quickfix item" },
+	},
+})
