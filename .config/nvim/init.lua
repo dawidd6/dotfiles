270b4357f2665cd4f1b6dd492f2a80f875877753
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-11 09:40:02 +0200

    nvim: lua -> plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,3 @@
 vim.loader.enable()
 
-local function require_all(directory)
-	for _, file in
-		ipairs(vim.fn.glob(string.format("%s/lua/%s/*.lua", vim.fn.stdpath("config"), directory), true, true))
-	do
-		require(string.format("%s.%s", directory, vim.fn.fnamemodify(file, ":t:r")))
-	end
-end
-
--- TODO: move files to plugin/ dir?
 require("vim._core.ui2").enable()
-require_all("core")
-require_all("extra")
