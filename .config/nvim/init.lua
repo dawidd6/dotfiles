4a71fcbea013dcdf201a90f25512659ce5e66c81
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-13 13:54:52 +0200

    nvim: use providers from browsher

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -308,16 +308,8 @@ do -- browsher.nvim
 
 	require("browsher").setup({
 		providers = {
-			["gitlab"] = {
-				url_template = "%s/-/blob/%s/%s",
-				single_line_format = "#L%d",
-				multi_line_format = "#L%d-%d",
-			},
-			["salsa"] = {
-				url_template = "%s/-/blob/%s/%s",
-				single_line_format = "#L%d",
-				multi_line_format = "#L%d-%d",
-			},
+			["gitlab"] = require("browsher").providers["gitlab.com"],
+			["salsa"] = require("browsher").providers["gitlab.com"],
 		},
 	})
 end
