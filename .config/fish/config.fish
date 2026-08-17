69409d5c0e9aa3d416a0e845682b82a98ed6d732
Dawid Dziurla <dawidd0811@gmail.com>
2026-08-17 11:19:38 +0200

    fish: add more windows paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,7 +21,7 @@ starship init fish | source
 zoxide init fish --cmd cd | source
 
 # PATH
-grep -qi microsoft /proc/sys/kernel/osrelease && fish_add_path --path --global --move /mnt/c/WINDOWS/system32
+grep -qi microsoft /proc/sys/kernel/osrelease && fish_add_path --path --global --move /mnt/c/WINDOWS/system32 /mnt/c/WINDOWS
 fish_add_path --path --global --move "$HOME/.bin"
 
 # Colors
