dad699cf3cfa2a1fc4b1c34c5ca6a3daff73d7f8
Dawid Dziurla <dawidd0811@gmail.com>
2026-09-09 08:37:00 +0200

    fish: wl-clipboard -n

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -65,8 +65,8 @@ bind \e\[1\;5C nextd-or-forward-word
 bind \e\[1\;5D prevd-or-backward-word
 
 # Abbrs
-abbr C wl-copy
-abbr P wl-paste
+abbr C 'wl-copy -n'
+abbr P 'wl-paste -n'
 abbr D 'diff-so-fancy | less --tabs 4 -RFX'
 abbr R 'exec fish'
 abbr r reset
